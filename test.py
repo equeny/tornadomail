b638bf29c00b8a0e4ee43d60a3a82fb994e5ba2d
Anton Agafonov <anton.agafonov@gmail.com>
2011-11-17 01:02:46 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,33 @@
+import contextlib
+
+from tornado import ioloop
+
+from tornadomail import send_mail
+from tornadomail.backends.smtp import EmailBackend
+
+from tornado import stack_context
+
+
+def callback(*args, **kwargs):
+    ioloop.IOLoop.instance().stop()
+    print 'Successfully sended'
+
+
+def error_handler(e, msg, traceback):
+    print 'Error:'
+    print msg
+    ioloop.IOLoop.instance().stop()
+    return True
+
+
+with stack_context.ExceptionStackContext(error_handler):
+    send_mail(
+        'subject', 'message', 'noreply.djangostars@gmail.com',
+        ['<your email>'], callback=callback,
+        connection=EmailBackend(
+            'smtp.gmail.com', 587, '<your google id>', '<your google password>',
+            True
+         )
+    )
+
+ioloop.IOLoop.instance().start()
---
+++ testproject.py
@@ -0,0 +1,62 @@
+#!/usr/bin/env python
+import logging
+
+import tornado.httpserver
+import tornado.ioloop
+import tornado.options
+import tornado.web
+from tornado import gen
+
+from tornado.options import define, options
+
+from tornadomail.message import EmailMessage
+from tornadomail.backends.smtp import EmailBackend
+
+define("port", default=8888, help="run on the given port", type=int)
+
+
+logging.basicConfig(level=logging.DEBUG) 
+
+class Application(tornado.web.Application):
+    mail = ''
+
+class MainHandler(tornado.web.RequestHandler):
+    @property
+    def mail_connection(self):
+        return EmailBackend(
+            'smtp.gmail.com', 587, '<your google id>', '<your google password>',
+            True
+         )
+
+    def get(self):
+        self.render("index.html")
+
+    def post(self):
+
+        def _finish(num):
+            print 'sended %d message(s)' % num
+            self.render("index.html")
+
+        message = EmailMessage(
+            self.get_argument('subject'),
+            self.get_argument('message'),
+            'noreply.djangostars@gmail.com',
+            [self.get_argument('email')],
+            connection=self.mail_connection
+        )
+        message.send()#callback=_finish)
+        self.render("index.html")
+
+
+def main():
+    tornado.options.parse_command_line()
+    application = Application([
+        (r"/", MainHandler),
+    ])
+    http_server = tornado.httpserver.HTTPServer(application)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
+
+
+if __name__ == "__main__":
+    main()
