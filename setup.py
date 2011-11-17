b1d6ce5637e1c9fba96366eecec85e76dac98ab6
Anton Agafonov <anton.agafonov@gmail.com>
2011-11-17 16:35:29 +0200

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import os
+from distutils.core import setup
+
+version = '0.1'
+
+setup(
+    name="tornadomail",
+    version=version,
+    keywords=["tornado", "mail", "tornadomail", "asyncmail"],
+    long_description=open(os.path.join(os.path.dirname(__file__),"README.md"), "r").read(),
+    description="Asynchronous email sending library for Tornado. Port of django.mail.",
+    author="Anton Agafonov",
+    author_email="anton.agafonov@gmail.com",
+    url="http://github.com/equeny/tornadomail",
+    license="Apache Software License",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+    ],
+    packages=['tornadomail'],
+    install_requires=['tornado>=2.1'],
+    requires=['tornado (>=1.9)'],
+    download_url="http://github.com/downloads/equeny/tornadomail/tornadomail-%s.tar.gz" % version,
+)
